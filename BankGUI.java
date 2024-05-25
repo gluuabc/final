a4fcb6e70a1f50857e736c95575dead40e915b81
stella li <120990066+gluuabc@users.noreply.github.com>
2024-05-25 12:53:29 -0700

    Update BankGUI.java

--- BankGUI.java
+++ BankGUI.java
@@ -1,139 +1,53 @@
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class BankGUI extends JFrame {
-    private JTextField accountNumberField;
-    private JTextField amountField;
-    private JTextArea transactionArea;
-    private Account currentAccount;
+public class Banking implements ActionListener {
+  private JFrame mainFrame;
+  private JTextField inputField;
+  private JLabel balanceLabel;
+  private double balance = 0.0;
 
-    public BankGUI() {
-        setTitle("Bank Account Management System");
-        setSize(400, 300);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new GridLayout(5, 2));
+  public Banking() {
 
-        accountNumberField = new JTextField();
-        amountField = new JTextField();
-        transactionArea = new JTextArea();
-        transactionArea.setEditable(false);
+    mainFrame = new JFrame("Simple Bank Application");
+    mainFrame.setVisible(true);
+    mainFrame.setSize(400, 400);
+    mainFrame.setFont(new Font("Arial",Font.BOLD,18));
+    mainFrame.setLayout(new FlowLayout());
 
-        JButton createSavingsButton = new JButton("Create Savings Account");
-        JButton createCheckingButton = new JButton("Create Checking Account");
-        JButton createLoanButton = new JButton("Create Loan Account");
-        JButton depositButton = new JButton("Deposit");
-        JButton withdrawButton = new JButton("Withdraw");
-        JButton transferButton = new JButton("Transfer");
-        JButton viewTransactionsButton = new JButton("View Transactions");
+    inputField = new JTextField(10);
+    balanceLabel = new JLabel("Current balance: " + balance);
+    JButton depositButton = new JButton("Deposit");
+    JButton withdrawButton = new JButton("Withdraw");
 
-        createSavingsButton.addActionListener(new CreateSavingsListener());
-        createCheckingButton.addActionListener(new CreateCheckingListener());
-        createLoanButton.addActionListener(new CreateLoanListener());
-        depositButton.addActionListener(new DepositListener());
-        withdrawButton.addActionListener(new WithdrawListener());
-        transferButton.addActionListener(new TransferListener());
-        viewTransactionsButton.addActionListener(new ViewTransactionsListener());
+    mainFrame.add(inputField);
+    mainFrame.add(depositButton);
+    mainFrame.add(withdrawButton);
+    mainFrame.add(balanceLabel);
 
-        add(new JLabel("Account Number:"));
-        add(accountNumberField);
-        add(new JLabel("Amount:"));
-        add(amountField);
-        add(createSavingsButton);
-        add(createCheckingButton);
-        add(createLoanButton);
-        add(depositButton);
-        add(withdrawButton);
-        add(transferButton);
-        add(viewTransactionsButton);
-        add(new JScrollPane(transactionArea));
+    depositButton.addActionListener(this);
+    withdrawButton.addActionListener(this);
 
-        setVisible(true);
+    mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    int amount = Integer.parseInt(inputField.getText());
+    if (e.getActionCommand().equals("Deposit")) {
+      balance += amount;
+    } else {
+      balance -= amount;
     }
+    balanceLabel.setText("Current balance: " + balance);
+    inputField.setText("");
+  }
 
-    private class CreateSavingsListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            String accountNumber = accountNumberField.getText();
-            currentAccount = new SavingsAccount(accountNumber, 0, 1.5);
-            transactionArea.append("Created Savings Account: " + accountNumber + "\n");
-        }
-    }
 
-    private class CreateCheckingListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            String accountNumber = accountNumberField.getText();
-            currentAccount = new CheckingAccount(accountNumber, 0);
-            transactionArea.append("Created Checking Account: " + accountNumber + "\n");
-        }
-    }
-
-    private class CreateLoanListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            String accountNumber = accountNumberField.getText();
-            currentAccount = new LoanAccount(accountNumber, 0, 5);
-            transactionArea.append("Created Loan Account: " + accountNumber + "\n");
-        }
-    }
-
-    private class DepositListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            if (currentAccount != null) {
-                double amount = Double.parseDouble(amountField.getText());
-                currentAccount.deposit(amount);
-                transactionArea.append("Deposited: $" + amount + "\n");
-            } else {
-                transactionArea.append("No account selected.\n");
-            }
-        }
-    }
-
-    private class WithdrawListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            if (currentAccount != null) {
-                double amount = Double.parseDouble(amountField.getText());
-                currentAccount.withdraw(amount);
-                transactionArea.append("Withdrew: $" + amount + "\n");
-            } else {
-                transactionArea.append("No account selected.\n");
-            }
-        }
-    }
-
-    private class TransferListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            // For simplicity, this example assumes transferring to a new checking account.
-            if (currentAccount != null) {
-                String toAccountNumber = JOptionPane.showInputDialog("Enter destination account number:");
-                double amount = Double.parseDouble(amountField.getText());
-                Account toAccount = new CheckingAccount(toAccountNumber, 0);
-                currentAccount.transfer(toAccount, amount);
-                transactionArea.append("Transferred: $" + amount + " to Account " + toAccountNumber + "\n");
-            } else {
-                transactionArea.append("No account selected.\n");
-            }
-        }
-    }
-
-    private class ViewTransactionsListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            if (currentAccount != null) {
-                currentAccount.viewTransactions();
-                transactionArea.append("Transactions viewed for Account " + currentAccount.getAccountNumber() + "\n");
-            } else {
-                transactionArea.append("No account selected.\n");
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        new BankGUI();
-    }
+  public static void main(String[] args) {
+    new Banking();
+  }
 }
+
+//yes
